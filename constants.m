5ac2296c668f63ca25511082408217954603d1ae
anapimenta1 <83237743+anapimenta1@users.noreply.github.com>
2022-01-25 19:59:41 +0000

    Constants to dynamics model

---
+++ constants.m
@@ -0,0 +1,19 @@
+% ENUNCIADO
+m = 810; % massa
+r = 0.256; % raio
+d = 1.1 ; % centro massa 
+L2 = 2.2;
+l = 0.64;
+
+% Actuator Parameters
+L = 0.125; % inductance
+R = 0.688; % resistance
+
+Kt = 11.43; % torque constant
+Kb = 0.06; % back emf constant
+
+J = 1000; % momento de inercia
+
+% PID 
+kp = 100; 
+ki = 0;
