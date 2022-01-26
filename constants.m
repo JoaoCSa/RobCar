30fff9a690487163fe11f713f6e0497030eff58d
FranciscoVasconcelos <37560515+FranciscoVasconcelos@users.noreply.github.com>
2022-01-26 19:24:13 +0000

    Update constants.m

--- constants.m
+++ constants.m
@@ -12,13 +12,35 @@ R = 0.688; % resistance
 Kt = 11.43; % torque constant
 Kb = 0.06; % back emf constant
 
-J = 1000; % momento de inercia
+J = 0.0025; % momento de inercia
 
 % PID 
-kp = 38.15; 
-ki = 66.99;
-kd = 4.377;
+kp = 100; 
+ki = 50;
+kd = 0;
 N = 18.3;
 
-trajectory = load('reference_tracking/new_traj.mat');
+trajectory = load('new_traj.mat');
 T = trajectory.tSamples(2);
+
+
+cont = kin_controller(trajectory);
+[OMEGAD, VD, THETAD] = cont.estimate_state_accel(trajectory.qd(1,:),trajectory.qd(2,:),trajectory.qdd(1,:),trajectory.qdd(2,:),T);
+
+xdot = trajectory.q(1,1) - trajectory.q(1,end);
+ydot = trajectory.q(2,1) - trajectory.q(2,end);
+theta_init = THETAD(1);
+%theta_init = atan2(ydot,xdot);
+%theta_init = pi;
+M = [r/2,r/2;r/l,-r/l];
+omegarl = M^(-1)*[VD;OMEGAD];
+omega1 = OMEGAD(1:end-1);
+omega2 =OMEGAD(2:end);
+
+v1 = VD(1:end-1);
+v2 = VD(2:end);
+vdot = (v1-v2)/T;
+omegadot = (omega1 - omega2)/T;
+
+x_init = trajectory.q(1,1);
+y_init = trajectory.q(2,1) - 10;
