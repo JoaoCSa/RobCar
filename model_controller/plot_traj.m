ea1134cac137903713a11d4f98c384b41dff1a25
ChopWater <83580237+ChopWater@users.noreply.github.com>
2022-01-30 20:49:29 +0000

    Update plot_traj.m

--- model_controller/plot_traj.m
+++ model_controller/plot_traj.m
@@ -6,9 +6,13 @@ end
 fig = [];
 fig = [fig;figure(1)];
 hold on;
-%reference trajectory
 plot(trajectory.q(1,:),trajectory.q(2,:),'.-');
 plot(out.x.Data,out.y.Data,'.-');
+title('Path Following');
+xlabel('x (m)');
+ylabel('y (m)');
+legend('reference','trajectory');
+axis equal;
 hold off;
 
 points1 = round(linspace(1,length(trajectory.tSamples),1000));
@@ -16,37 +20,31 @@ points2 = round(linspace(1,length(out.y.Data),5000));
 
 fig = [fig;figure(2)];
 hold on;
-%vetores com as velocidades e dire;ao
 quiver(trajectory.q(1,points1),trajectory.q(2,points1),trajectory.qd(1,points1),trajectory.qd(2,points1),0.5);
 quiver(out.x.Data(points2),out.y.Data(points2),out.x_dot.Data(points2),out.y_dot.Data(points2), 0.5);
+title('Linear Velocity Vectors');
+xlabel('x (m)');
+ylabel('y (m)');
+legend('reference','trajectory');
 hold off;
 
 
 fig = [fig;figure(3)];
 hold on;
-%evolucao x y ao longo do tempo
-plot(out.x.Time,out.x.Data);
-plot(out.y.Time,out.y.Data);
+plot(out.wref.Time,out.wref.Data);
+plot(out.w.Time,out.w.Data);
+title('Angular Velocity');
+xlabel('Time (s)');
+ylabel('Angular Velocity (rad/s)');
+legend('reference','simulated');
 hold off;
 
 fig = [fig;figure(4)];
 hold on;
-%THETA E OMEGA
-plot(trajectory.tSamples,THETAD);
-plot(trajectory.tSamples,OMEGAD);
-hold off;
-
-
-fig = [fig;figure(5)];
-hold on;
-%velocidade x y
-plot(out.x_dot.Time,out.x_dot.Data);
-plot(out.y_dot.Time,out.y_dot.Data);
-hold off;
-
-%veolicade linear e referencia
-%velocidade angualr e referencia
-fig = [fig;figure(6)];
-hold on;
-plot(VD)
+plot(out.vref.Time,out.vref.Data);
+plot(out.v.Time,out.v.Data);
+title('Linear Velocity');
+xlabel('Time (s)');
+ylabel('Velocity (m/s)');
+legend('reference','simulated');
 hold off;
